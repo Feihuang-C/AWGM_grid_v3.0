3d3d754e805557119505b0e37d1950a00062c537
Feihuang-C <51823498+Feihuang-C@users.noreply.github.com>
2023-07-24 23:30:21 +0800

    Update job_submit.sh

--- src/job_submit.sh
+++ src/job_submit.sh
@@ -13,12 +13,11 @@ echo 'making bklist.txt'
 bklist='./bklist.txt'
 ls -d 0* >> $bklist
 
-echo 'making input.lst'
 for diri in `cat $bklist`
 do
 cp ./DispInv.sh $diri/DispInv.sh
 cd $diri
 ./DispInv.sh
 rm ./DispInv.sh
-cd ..
+cd ../
 done
